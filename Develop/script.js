5516a5ba33cb71f98bc49177374ef3988ccb3bb4
Hussein <hussein.usa@outlook.com>
2022-04-06 14:56:48 -0400

    finiliaze generate function

--- Develop/script.js
+++ Develop/script.js
@@ -27,20 +27,59 @@ function generatePassword(){
   // Ask the user about the lenght
     var lenght = prompt ( " how long do you need your password to be? Choose between 8 and 128 characters. " );
 
-    // Loop if the answer isn't between 8 and 128
-    while (lenght < 8 && lenght > 128){
-      alert( " Your password should contain no more than 128 and no less than 8 characters");
-      var lenght = (prompt( " How long do you need your password to be? Choose between 8 and 128 charcters. " );
-    }
+      // Loop if the answer isn't between 8 and 128
+      while (lenght < 8 && lenght > 128){
+        alert( " Your password should contain no more than 128 and no less than 8 characters");
+        var lenght = (prompt( " How long do you need your password to be? Choose between 8 and 128 charcters. " ) );
+      }
+      // Tell the user how many characters he chose
+      alert( " Would you like your password to have: " + lenght + " characters? " );
 
-    // Tell the user how many characters he chose
-    alert( " Would you like your password to have: " + lenght + " characters? " )
 
-    //Confirm the conditions of the password with the user
+  //Confirm the conditions of the password with the user
     var confirmlwr  = confirm ( " Do you want to include Lower Case Letters? " );
     var confirmuppr = confirm ( " Do you want to include Upper Case Letters? " );
     var confirmnum  = confirm ( " Do you want to include Numbers?  " );
     var confirmspec = confirm ( " Do you want to include special Characters? " );
+
+      // Loop if all conditions were not found
+      while ( confirmlwr === false && confirmuppr === false && confirmnum === false && confirmspec === false ){
+        alert ( " At lease one character type should be selected " );
+          var confirmlwr  = confirm ( " Do you want to include Lower Case Letters? " );
+          var confirmuppr = confirm ( " Do you want to include Upper Case Letters? " );
+          var confirmnum  = confirm ( " Do you want to include Numbers?  " );
+          var confirmspec = confirm ( " Do you want to include special Characters? " );
+
+      }
+
+      //conditions for the password
+      var passcond = []
+
+      if (confirmuppr){
+        passcond = passcond.concat(uppr)
+      }
+      if (confirmwlr){
+        passcond = passcond.concat(lwr)
+      }
+      if (confirmnum){
+        passcond = passcond.concat(num)
+      }
+      if (confirmspec){
+        passcond = passcond.concat(spec)
+      }
+
+      console.log (passcond)
+
+  // if its empty
+
+  var random = ""
+
+      for ( var i=0 ; i<lenght ; i++){
+        random = random + passcond [ Math.floor(Math.random() + passcond.lenght ) ];
+        console.log ( random )
+      }
+      return random;
+
 
 }
 
